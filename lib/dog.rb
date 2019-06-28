86233c881a74fbe715ee9b9cab36d9a050cd05b2
Sanjeev Tamang <sanzeevtamang@gmail.com>
2019-06-28 11:21:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Dog 
+  def intialize(name,bark)
+    @name =name 
+    @bark = bark
+  end 
+  
+  def nameg #getter
+    @name
+  end 
+  
+  def names(name) #setter
+    @name = name
+  end 
+  
+  def barkg
+    @bark 
+  end 
+  
+  def barks(bark)
+    @bark = bark 
+  end 
+  
+end
